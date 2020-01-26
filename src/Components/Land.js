85dec20076786d9f5120fa76ac93d560f8cdd40b
Robert Nadal <rmnadal@edu.uwaterloo.ca>
2020-01-26 10:14:05 -0500

    Beautified landing page

--- src/Components/Land.js
+++ src/Components/Land.js
@@ -37,40 +37,42 @@ export default function Land() {
             <div className="body">
                 <div className="platform-body">
                     OUR PLATFORM
-                </div>                
-                <div className="title-card">
-                    <div>
-                        <i class="material-icons title-icon"> search </i>
-                    </div>                    
-                    <div className="icon-subtext">
-                        COMPANY ETHICS
-                    </div>
-                    <div className="subtext-info">
-                        Employees deliver numeric reports on the ethics of their companies
-                    </div>
                 </div>
-                <div className="title-card">
-                    <div>
-                        <i class="material-icons title-icon"> bar_chart </i>
-                    </div>                    
-                    <div className="icon-subtext">
-                        DATA ANALYTICS
+                <div className="info-cards">
+                    <div className="title-card">
+                        <div>
+                            <i class="material-icons title-icon"> search </i>
+                        </div>                    
+                        <div className="icon-subtext">
+                            COMPANY ETHICS
+                        </div>
+                        <div className="subtext-info">
+                            Employees deliver numeric reports on the ethics of their companies
+                        </div>
                     </div>
-                    <div className="subtext-info">
-                        Scrutible analyzes reports and generates ethical credit scores
+                    <div className="title-card">
+                        <div>
+                            <i class="material-icons title-icon"> bar_chart </i>
+                        </div>                    
+                        <div className="icon-subtext">
+                            DATA ANALYTICS
+                        </div>
+                        <div className="subtext-info">
+                            Scrutible analyzes reports and generates ethical credit scores
+                        </div>
                     </div>
-                </div>
-                <div className="title-card">
-                    <div>
-                        <i class="material-icons title-icon"> message </i>
-                    </div>                    
-                    <div className="icon-subtext">
-                        USER FEEDBACK
+                    <div className="title-card">
+                        <div>
+                            <i class="material-icons title-icon"> message </i>
+                        </div>                    
+                        <div className="icon-subtext">
+                            USER FEEDBACK
+                        </div>
+                        <div className="subtext-info">
+                            Consumers and public provide extra qualitative data through comments
+                        </div>
                     </div>
-                    <div className="subtext-info">
-                        Consumers and public provide extra qualitative data through comments
-                    </div>
-                </div>
+                </div>            
             </div>
         </div>
     );
