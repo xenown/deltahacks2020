dd33d1212f8883fa734bf95fd252d0ad555c1cf0
Robert Nadal <rmnadal@edu.uwaterloo.ca>
2020-01-26 02:24:30 -0500

    started landing page title

--- src/Components/Land.js
+++ src/Components/Land.js
@@ -1,48 +1,76 @@
 import React from 'react';
 import "./Land.css";
 import Typography from "@material-ui/core/Typography"
-import { makeStyles } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core';
 // import background from "../sustainability.jpg";
 
-const useStyles = makeStyles(theme => ({
-    title: {
-        "margin": 0,
-        "position": "absolute",
-        "top": "50%",
-        "left": "11%",
-        "margin-right": "-90%",
-        "transform": "translate(-50%, -150%)",
-        "color": "white"
-    },
-    blurb: {
-        "margin": 0,
-        "position": "absolute",
-        "top": "50%",
-        "left": "30%",
-        "margin-right": "-70%",
-        "transform": "translate(-50%, -50%)",
-        "color": "white"
-    }
-}
-));
+// const useStyles = makeStyles(theme => ({
+//     title: {
+//         "position": "absolute",
+//         "top": "50%",
+//         "left": "20%",
+//         "transform": "translate(-50%, -150%)",
+//         "color": "white"
+//     },
+//     blurb: {
+//         "position": "absolute",
+//         "top": "50%",
+//         "left": "50%",
+//         "transform": "translate(-50%, -50%)",
+//         "color": "white"
+//     }
+// }
+// ));
 
 export default function Land() {
-    const classes = useStyles();
+    // const classes = useStyles();
     return (
         <div className="land">
             <div className="main" >
-                <Typography className={classes.title} variant="h2" noWrap>
-                    Scrutable
-                </Typography>
-                <Typography className={classes.blurb} variant="h3" noWrap>
+            <div className="title-subtext" variant="h3" noWrap>
                     Scrutanize the makers of your consumables.
-                </Typography>
+                </div>
+                <div className="title-header" variant="h2" noWrap>
+                    SCRUTABLE
+                </div>                
             </div>
             <div className="body">
-                <Typography className={classes.body} variant="h5" noWrap>
-                    In a world where market share can drop by 15% because of a tainted supply chain, a de-facto standard for ethical compliance is needed more than ever.
- 
-                </Typography>
+                <div className="platform-body">
+                    OUR PLATFORM
+                </div>                
+                <div className="title-card">
+                    <div>
+                        <i class="material-icons title-icon"> search </i>
+                    </div>                    
+                    <div className="icon-subtext">
+                        COMPANY ETHICS
+                    </div>
+                    <div className="subtext-info">
+                        Employees deliver numeric reports on the ethics of their companies
+                    </div>
+                </div>
+                <div className="title-card">
+                    <div>
+                        <i class="material-icons title-icon"> bar_chart </i>
+                    </div>                    
+                    <div className="icon-subtext">
+                        DATA ANALYTICS
+                    </div>
+                    <div className="subtext-info">
+                        Scrutible analyzes reports and generates ethical credit scores
+                    </div>
+                </div>
+                <div className="title-card">
+                    <div>
+                        <i class="material-icons title-icon"> message </i>
+                    </div>                    
+                    <div className="icon-subtext">
+                        USER FEEDBACK
+                    </div>
+                    <div className="subtext-info">
+                        Consumers and public provide extra qualitative data through comments
+                    </div>
+                </div>
             </div>
         </div>
     );
